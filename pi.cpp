0ff3d2cc4821f1a2a57199ab01c71698706e1df3
Chinthan23 <77500564+Chinthan23@users.noreply.github.com>
2022-04-07 21:17:27 +0530

    Done

--- pi.cpp
+++ pi.cpp
@@ -2,57 +2,62 @@
 
 int main()
 {
-    ll pr=1000;
-    pair<vector<ll>,ll> root2=sqaureroot({{2},0},pr+5);
-    pair<vector<ll>,ll> a=root2;
-    pair<vector<ll>,ll> b={{0},0};
-    pair<vector<ll>,ll> p=add({{2},0},root2);
+    ll pr=2000;
+    pair<vector<ll>,ll> root2,a,b,p,two;
     pair<vector<ll>,ll> half={{0,B/2},1};
+    two={{2},0};
+    root2=sqaureroot(two,pr+5);
+    a=root2;
+    b={{0},0};
+    p=add({{2},0},root2);
+    pair<vector<ll>,ll> a_1,a_2,a_3,anew;
+    pair<vector<ll>,ll> b_1,b_2,b_3,b_4,bnew;
+    pair<vector<ll>,ll> p_1,p_2,p_3,p_4,p_5,pnew,check;
     ll c=0;
     bool checker=false;
     ll pri=4;
     while(true)
     {
-        pair<vector<ll>,ll> a_1=sqaureroot(a,pri);
-        pair<vector<ll>,ll> a_2=divd({{1},0},a_1,pri);
-        pair<vector<ll>,ll> a_3=add(a_1,a_2);
-        pair<vector<ll>,ll> anew=multiplicationd(half,a_3);
+        a_1=sqaureroot(a,pri);
+        a_2=divd({{1},0},a_1,pri);
+        a_3=add(a_1,a_2);
+        anew=multiplicationd(a_3,half);
         reverse(anew.first.begin(),anew.first.end());
         truncate(anew.first);
         reverse(anew.first.begin(),anew.first.end());
         truncated(anew);
-        pair<vector<ll>,ll> b_1=add({{1},0},b);
-        pair<vector<ll>,ll> b_2=multiplicationd(b_1,a_1);
+        b_1=add({{1},0},b);
+        b_2=multiplicationd(b_1,a_1);
         reverse(b_2.first.begin(),b_2.first.end());
         truncate(b_2.first);
         reverse(b_2.first.begin(),b_2.first.end());
         truncated(b_2);
-        pair<vector<ll>,ll> b_3=add(a,b);
-        pair<vector<ll>,ll> b_4=divd({{1},0},b_3,pri);
-        pair<vector<ll>,ll> bnew=multiplicationd(b_2,b_4);
+        b_3=add(a,b);
+        b_4=divd({{1},0},b_3,pri);
+        bnew=multiplicationd(b_2,b_4);
         reverse(bnew.first.begin(),bnew.first.end());
         truncate(bnew.first);
         reverse(bnew.first.begin(),bnew.first.end());
         truncated(bnew);
-        pair<vector<ll>,ll> p_1=add({{1},0},anew);
-        pair<vector<ll>,ll> p_2=multiplicationd(p_1,p);
+        p_1=add({{1},0},anew);
+        p_2=multiplicationd(p_1,p);
         reverse(p_2.first.begin(),p_2.first.end());
         truncate(p_2.first);
         reverse(p_2.first.begin(),p_2.first.end());
         truncated(p_2);
-        pair<vector<ll>,ll> p_3=multiplicationd(p_2,bnew);
+        p_3=multiplicationd(p_2,bnew);
         reverse(p_3.first.begin(),p_3.first.end());
         truncate(p_3.first);
         reverse(p_3.first.begin(),p_3.first.end());
         truncated(p_3);
-        pair<vector<ll>,ll> p_4=add({{1},0},bnew);
-        pair<vector<ll>,ll> p_5=divd({{1},0},p_4,pri);
-        pair<vector<ll>,ll> pnew=multiplicationd(p_3,p_5);
+        p_4=add({{1},0},bnew);
+        p_5=divd({{1},0},p_4,pri);
+        pnew=multiplicationd(p_3,p_5);
         reverse(pnew.first.begin(),pnew.first.end());
         truncate(pnew.first);
         reverse(pnew.first.begin(),pnew.first.end());
         truncated(pnew);
-        pair<vector<ll>,ll> check=subtractiond(pnew,p);
+        check=subtractiond(pnew,p);
         if(check.second>pr)
         {
             bool icheck=true;
